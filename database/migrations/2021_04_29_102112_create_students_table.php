82e10580a335bbc9ffcb5a151a766c7cbec883f0
Marko Ivanovic <marko.ivanovic.dev@gmail.com>
2021-04-29 12:43:31 +0200

    added students migration

---
+++ database/migrations/2021_04_29_102112_create_students_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('board');
+            $table->string('name');
+            $table->integer('grade_1')->nullable();
+            $table->integer('grade_2')->nullable();
+            $table->integer('grade_3')->nullable();
+            $table->integer('grade_4')->nullable();
+            $table->float('average');
+            $table->string('final');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
